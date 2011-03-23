1b349c655b17ca6de27662629ec83deb5150705c
estama <estama@servum>
2011-03-23 14:20:23 +0200

    Changed Mac recognition

--- src/lib/pyperclip.py
+++ src/lib/pyperclip.py
@@ -111,7 +111,7 @@ if os.name == 'nt':
     import ctypes
     getcb = winGetClipboard
     setcb = winSetClipboard
-elif os.name == 'mac':
+elif lower(os.uname[0]) == 'darwin':
     getcb = macGetClipboard
     setcb = macSetClipboard
 elif os.name == 'posix':
