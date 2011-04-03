606e5a88bf2d9a02288c2c23d4b3c8d62c4e266b
estama <estama@xola>
2011-04-03 22:12:33 +0300

    Changed create to return int if feeded a single int. Added more doctests

--- src/lib/jgroup.py
+++ src/lib/jgroup.py
@@ -4,11 +4,25 @@ This is the jgroup module
 It features conversion to and from jlists
 
 >>> create(3)
-'[3]'
+3
+>>> create('3')
+'3'
 >>> create('test')
 'test'
 >>> create('[testjsonlike]')
-'"[testjsonlike]"'
+'["[testjsonlike]"]'
+>>> create('[testjsonlike')
+'[testjsonlike'
+>>> create([3])
+3
+>>> create(['test'])
+'test'
+>>> create(['test',3])
+'["test", 3]'
+>>> create([3,'test'])
+'[3, "test"]'
+>>> create(['[test'])
+'[test'
 
 """
 
@@ -16,26 +30,29 @@ import json
 
 def create(l):
     typel=type(l)
-    if typel==list:
-        lenl=len(l)
-        if lenl==1:
-            if type(l[0])==string:
-                if len(l[0])==0:
-                    return u''
-                elif  l[0][0]!='[' or l[0][-1]!=']':
-                    return l[0]
-        if lenl==0:
-            return ''
-        return json.dumps(l)
     if typel==str:
-        if len(l)==0:
+        if l=='':
             return u''
         elif l[0]!='[' or l[-1]!=']':
             return l
         else:
-            return json.dumps(l)
+            return json.dumps([l])
     if typel==int:
-        return json.dumps([l])
+        return l
+    if typel==list:
+        lenl=len(l)
+        if lenl==1:
+            if type(l[0])==str:
+                if l[0]=='':
+                    return u''
+                elif  l[0][0]!='[' or l[0][-1]!=']':
+                    return l[0]
+            if type(l[0])==int:
+                return l[0]
+        if lenl==0:
+            return ''
+        return json.dumps(l)
+
 
 def createstrict(l):
     return json.dumps(l)
