9d4d1409d85a4a94418f65f692bec3f76ca52ba3
estama <estama@servum>
2011-04-04 16:02:11 +0300

    Initial version of jpacks

---
+++ src/functions/row/jpacks.py
@@ -0,0 +1,176 @@
+import lib.jlist as jlist
+
+def jpack(*args):
+
+    """
+    .. function:: jpack(args...) -> str
+
+    Converts multiple input arguments into a single string. Jpacks preserve the types
+    of their inputs and are based on JSON encoding. Single values are represented as
+    themselves where possible.
+
+    Examples:
+
+    >>> sql("select jpack('a')")
+    jpack('a')
+    ----------
+    a
+
+    >>> sql("select jpack('a','b',3)")
+    jpack('a','b',3)
+    ----------------
+    ["a", "b", 3]
+
+    """
+
+    return jlist.toj(args)
+
+jpack.registered=True
+
+def j2tpack(*args):
+
+    """
+    .. function:: j2tpack(args) -> str
+
+    Converts multiple input jpacks to a tab pack (tab separated values). If tab characters are found in
+    the source jpack
+
+    Examples:
+
+    >>> sql("select j2tpack('[1,2,3]')") # doctest: +NORMALIZE_WHITESPACE
+    j2tpack('[1,2,3]')
+    ------------------
+    1        2        3
+
+    >>> sql("select j2tpack('[1,2,3]','a')") # doctest: +NORMALIZE_WHITESPACE
+    j2tpack('[1,2,3]','a')
+    ----------------------
+    1        2        3        a
+
+    >>> sql("select j2tpack('a', 'b')") # doctest: +NORMALIZE_WHITESPACE
+    j2tpack('a', 'b')
+    -----------------
+    a        b
+
+    """
+
+    fj=[]
+    for j in args:
+        fj+=[ str(x).replace('\t', '    ') for x in jlist.fromj(j) ]
+        
+    return '\t'.join(fj)
+
+j2tpack.registered=True
+
+def t2jpack(*args):
+
+    """
+    .. function:: t2jpack(args) -> str
+
+    Converts a tab pack to a jpack.
+
+    Examples:
+
+    >>> sql("select t2jpack(j2tpack('[1,2,3]'))") # doctest: +NORMALIZE_WHITESPACE
+    t2jpack(j2tpack('[1,2,3]'))
+    ---------------------------
+    ["1", "2", "3"]
+
+    """
+    
+    fj=[]
+    for t in args:
+        fj+=t.split('\t')
+
+    return jlist.toj(fj)
+
+t2jpack.registered=True
+
+def jmerge(*args):
+
+    """
+    .. function:: jmerge(args) -> str
+
+    Merges multiple jpacks into one jpack.
+
+    Examples:
+
+    >>> sql("select jmerge('[1,2,3]', '[1,2,3]', 'a', 3 )") # doctest: +NORMALIZE_WHITESPACE
+    jmerge('[1,2,3]', '[1,2,3]', 'a', 3 )
+    -------------------------------------
+    [1, 2, 3, 1, 2, 3, "a", 3]
+
+    """
+
+    fj=[]
+    for j in args:
+        fj+=jlist.fromj(j)
+
+    return jlist.toj(fj)
+
+jmerge.registered=True
+
+def jset(*args):
+
+    """
+    .. function:: jset(args) -> str
+
+    Returns a set representation of a jpack, unifying duplicate items.
+
+    Examples:
+
+    >>> sql("select jset('[1,2,3]', '[1,2,3]', 'b', 'a', 3 )") # doctest: +NORMALIZE_WHITESPACE
+    jset('[1,2,3]', '[1,2,3]', 'b', 'a', 3 )
+    ----------------------------------------
+    [1, 2, 3, "a", "b"]
+
+    """
+
+    fj=[]
+    for j in args:
+        fj+=jlist.fromj(j)
+
+    return jlist.toj(sorted(set(fj)))
+
+jset.registered=True
+
+def jsort(*args):
+
+    """
+    .. function:: jset(args) -> str
+
+    Returns a set representation of a jpack, unifying duplicate items.
+
+    Examples:
+
+    >>> sql("select jsort('[1,2,3]', '[1,2,3]', 'b', 'a', 3 )") # doctest: +NORMALIZE_WHITESPACE
+    jsort('[1,2,3]', '[1,2,3]', 'b', 'a', 3 )
+    -----------------------------------------
+    [1, 1, 2, 2, 3, 3, 3, "a", "b"]
+
+    """
+
+    fj=[]
+    for j in args:
+        fj+=jlist.fromj(j)
+
+    return jlist.toj(sorted(fj))
+
+jsort.registered=True
+
+
+
+if not ('.' in __name__):
+    """
+    This is needed to be able to test the function, put it at the end of every
+    new function you create
+    """
+    import sys
+    import setpath
+    from functions import *
+    testfunction()
+    if __name__ == "__main__":
+        reload(sys)
+        sys.setdefaultencoding('utf-8')
+        import doctest
+        doctest.testmod()
