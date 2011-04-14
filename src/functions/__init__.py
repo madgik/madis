2adf82c2f573c03382fecb555c6fcb91a5ddf3be
estama <estama@servum>
2011-04-14 14:13:36 +0300

    Reverting last commit

--- src/functions/__init__.py
+++ src/functions/__init__.py
@@ -339,7 +339,7 @@ def register_ops(module, connection):
                 functions['row'][opname] = fobject
                 if isgenerator(fobject):
                     fobject=wraprowiter(connection, opname)
-                    multiset_functions[opname]=True
+                    fobject.multiset=True
                 setattr(rowfuncs, opname, fobject)
                 connection.createscalarfunction(opname, fobject)
 
@@ -350,13 +350,16 @@ def register_ops(module, connection):
                 if isgenerator(fobject.final):
                     fobject.__iterated_final__=fobject.final
                     fobject.final=wrapagriter(connection, opname)
-                    multiset_functions[opname]=True
+                    fobject.multiset=True
 
                 setattr(fobject,'factory',classmethod(lambda cls:(cls(), cls.step, cls.final)))
                 connection.createaggregatefunction(opname, fobject.factory)
 
-            if hasattr(fobject, 'multiset') and type(fobject.multiset).__name__ == 'bool' and fobject.multiset == True:
-                    multiset_functions[opname]=True
+            try:
+                if fobject.multiset == True:
+                        multiset_functions[opname]=True
+            except:
+                pass
 
 def testfunction():
     global test_connection, settings
