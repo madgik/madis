fd564efc29978f10558056a2c044a95d580afbc4
estama <estama@servum>
2011-04-15 13:43:33 +0300

    Changed setpaths to os.path.abspath

--- src/functions/aggregate/setpath.py
+++ src/functions/aggregate/setpath.py
@@ -1,3 +1,3 @@
 import sys, os
 curmodulepath = os.path.dirname( os.path.abspath(__file__) )
-sys.path.append((os.path.join(curmodulepath,'..','..')))
+sys.path.append(os.path.abspath(os.path.join(curmodulepath,'..','..')))
