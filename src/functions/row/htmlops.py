e1dba290a7f9e5fce7f2a9e403095f87ae46460b
estama <estama@servum>
2011-05-20 19:25:52 +0300

    -Moved url functions from fileurlops to htmlops -Renamed fileurlops to fileops -Added htmladdbreaks in htmlops

--- src/functions/row/htmlops.py
+++ src/functions/row/htmlops.py
@@ -3,6 +3,93 @@ import urllib
 import re
 from htmlentitydefs import name2codepoint
 name2codepoint['#39'] = 39
+import urlparse
+import os
+import mimetypes
+import xml.sax.saxutils
+
+def urlsplit(*args):
+
+    """
+    .. function:: urlsplit(text1, [text2,...]) -> multiset
+
+    Breaks a given URL into multiple fields. The returned table schema is:
+
+    :scheme: What type the URL is (e.g. http, ftp ...)
+    :netloc: Network location of URL (e.g. www.text.com)
+    :path: Path part of URL (e.g. /data/2010/). It always has a slash at the end
+    :filename: Filename part of URL
+    :type: Mime type of URL, or if not a mime type exists, the extension part of filename.
+    :subtype: Mime subtype of URL.
+    :params: All parameters following ';' in URL.
+    :query: All parameters following '?' in URL.
+    :fragment: All parameters following '#' in URL.
+
+    Examples:
+
+    >>> table1('''
+    ... http://www.test.com/apath/bpath/fname.pdf
+    ... http://www.test.com/search.csv;p=5?q=test#hl=en
+    ... ''')
+    >>> sql("select urlsplit(a) from table1")
+    scheme | netloc       | path          | filename   | type        | subtype | params | query  | fragment
+    -------------------------------------------------------------------------------------------------------
+    http   | www.test.com | /apath/bpath/ | fname.pdf  | application | pdf     |        |        |
+    http   | www.test.com | /             | search.csv | csv         |         | p=5    | q=test | hl=en
+    """
+
+    yield ('scheme', 'netloc', 'path', 'filename', 'type', 'subtype', 'params', 'query', 'fragment')
+
+    url=''.join(args)
+    u=urlparse.urlparse(''.join(args))
+    pf=os.path.split(u[2])
+
+    if len(pf)==2:
+        path, filename=pf
+    else:
+        path, filename=pf[0], ''
+
+    if len(path)>0 and path[-1]!='/':
+        path+='/'
+
+    m=mimetypes.guess_type(url)
+    if m[0]!=None:
+        m1, m2=m[0].split('/')
+    else:
+        m1, m2=(os.path.splitext(filename)[1], '')
+        if len(m1)>0 and m1[0]=='.':
+            m1=m1[1:]
+
+    yield [u[0], u[1], path, filename, m1, m2, u[3], u[4], u[5]]
+
+urlsplit.registered=True
+urlsplit.multiset=True
+
+def urllocation(*args):
+
+    """
+    .. function:: urllocation(str) -> str
+
+    Returns the location part of provided URL.
+
+    Examples:
+
+    >>> table1('''
+    ... http://www.test.com/apath/bpath/fname.pdf
+    ... http://www.test.com/search.csv;p=5?q=test#hl=en
+    ... ''')
+    >>> sql("select urllocation(a) from table1")
+    urllocation(a)
+    -----------------------------------------
+    http://www.test.com/apath/bpath/fname.pdf
+    http://www.test.com/search.csv
+    """
+
+    u=urlparse.urlparse(''.join(args))
+
+    return u[0]+u'://'+''.join(u[1:3])
+
+urllocation.registered=True
 
 def htmlunescape(s):
     return re.sub('&(%s);' % '|'.join(name2codepoint),
@@ -32,6 +119,32 @@ def htmldecode(*args):
     return htmlunescape(args[0])
 
 htmldecode.registered=True
+
+def htmlencode(*args):
+    """
+    .. function:: htmldecode(str)
+
+    Returns the html decoded *str*.
+
+    Examples:
+
+    >>> sql("select htmldecode('(&quot;die+wunderbaren+jahre&quot;)') as query")
+    query
+    -------------------------
+    ("die+wunderbaren+jahre")
+    >>> sql("select htmldecode(null) as query")
+    query
+    -----
+    None
+    """
+    if len(args)>1:
+        raise functions.OperatorError("htmldecode","operator takes only one argument")
+    if args[0]==None:
+        return None
+
+    return xml.sax.saxutils.escape(u''.join(args[0]), {'"': "&quot;"})
+
+htmlencode.registered=True
 
 def urldecode(*args):
     """
@@ -82,7 +195,7 @@ def urlencode(*args):
 
 urlencode.registered=True
 
-def url(*args):
+def htmladdbreaks(*args):
     """
     .. function:: url(href, linktext)
 
@@ -90,17 +203,45 @@ def url(*args):
 
     Examples:
 
-    >>> sql("select url('http://somewhere.org') as url")
-    url
-    -------------------------------------------------------
-    <a href="http://somewhere.org">http://somewhere.org</a>
+    >>> sql("select htmladdbreaks('very-long/string') as brokenhtml")
+    brokenhtml
+    ----------------------
+    very-​long/​string
+    """
 
-    >>> sql("select url('somewhere.org') as url")
-    url
-    ------------------------------------------------
-    <a href="http://somewhere.org">somewhere.org</a>
+    if args[0]==None:
+        return None
 
-    >>> sql("select url('somewhere.org', 'go somewhere') as url")
+    breakchars=['/','.','-']
+
+    out=u''.join(args)
+
+    for i in breakchars:
+        out=out.replace(i,i+u'\u200B')
+
+    return out
+
+htmladdbreaks.registered=True
+
+def htmllink(*args):
+    """
+    .. function:: htmllink(href, linktext)
+
+    Returns the an html link pointing to *href* and having the link text *linktext*.
+
+    Examples:
+
+    >>> sql("select htmllink('http://somewhere.org') as url") #doctest:+ELLIPSIS +NORMALIZE_WHITESPACE
+    url
+    ----------------------------------------------------------------
+    <a href="http://somewhere.org">http:/​/​somewhere.​org</a>
+
+    >>> sql("select htmllink('somewhere.org') as url")
+    url
+    ---------------------------------------------------
+    <a href="http://somewhere.org">somewhere.​org</a>
+
+    >>> sql("select htmllink('somewhere.org', 'go somewhere') as url")
     url
     -----------------------------------------------
     <a href="http://somewhere.org">go somewhere</a>
@@ -120,9 +261,9 @@ def url(*args):
 
     if args[0]==None:
         return None
-    return '<a href="'+addhttp(args[0])+'">'+unicode(args[0])+'</a>'
+    return '<a href="'+addhttp(args[0])+'">'+htmladdbreaks(htmlencode(unicode(args[0])))+'</a>'
 
-url.registered=True
+htmllink.registered=True
 
 if not ('.' in __name__):
     """
