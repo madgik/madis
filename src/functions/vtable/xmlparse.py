866dfb7e0abea774f8fdbb205041ae503bda5a03
estama <estama@servum>
2011-12-28 20:07:11 +0200

    -Slow path unescapes html entities

--- src/functions/vtable/xmlparse.py
+++ src/functions/vtable/xmlparse.py
@@ -154,6 +154,7 @@ Examples:
 import vtiters
 import functions
 import collections
+import htmlentitydefs
 import json
 import cStringIO as StringIO
 
@@ -170,6 +171,29 @@ attribguard='@'
 # Workaround for older versions of elementtree
 if not hasattr(etree, 'ParseError'):
     etree.ParseError=etree.XMLParserError
+
+def unescape(text):
+    def fixup(m):
+        text = m.group(0)
+        if text=='&amp;': return text
+        if text[:2] == "&#":
+            # character reference
+            try:
+                if text[:3] == "&#x":
+                    return unichr(int(text[3:-1], 16))
+                else:
+                    return unichr(int(text[2:-1]))
+            except ValueError:
+                pass
+        else:
+            # named entity
+            try:
+                text = unichr(htmlentitydefs.name2codepoint[text[1:-1]])
+            except KeyError:
+                pass
+        print text
+        return text # leave as is
+    return re.sub("&#?\w+;", fixup, text)
 
 def matchtag(a, b):
     if b[0] == '{':
@@ -526,7 +550,7 @@ class XMLparse(vtiters.SchemaFromArgsVT):
                         return "<xmlparce-forced-root-element>\n"+self.lastline
 
             def readtail(self, n):
-                line= self.qiter.next()[0].encode('utf-8')
+                line= unescape(self.qiter.next()[0]).encode('utf-8')
                 if line.startswith('<?xml version='):
                     line= self.qiter.next()[0].encode('utf-8')
                 if not line.endswith('\n'):
