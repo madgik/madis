547d90ea2f98a7cc3deb653be5253ec24aba39a6
estama <estama@xola>
2013-12-08 15:18:34 +0200

    Supporting more OSes

--- src/functions/vtable/pipe.py
+++ src/functions/vtable/pipe.py
@@ -58,7 +58,7 @@ class PipeVT(vtbase.VT):
 
         if linesplit:
             for line in iter(child.stdout.readline, ''):
-                yield [line[:-1].decode('utf-8', errors='replace')]
+                yield [line.rstrip("\r\n").decode('utf-8', errors='replace')]
         else:
             output, error = child.communicate()
             if child.returncode!=0:
