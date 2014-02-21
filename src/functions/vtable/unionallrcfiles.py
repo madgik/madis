5317ce2eeb16cf7378a0a664d984eba9ac101802
johnfouf <johnfouf@johnfouf-Z68P-DS3>
2014-02-21 13:59:41 +0200

    unionallrcfiles debug

--- src/functions/vtable/unionallrcfiles.py
+++ src/functions/vtable/unionallrcfiles.py
@@ -97,7 +97,7 @@ class UnionAllRC(vtbase.VT):
                     try:
                         ind = struct.unpack(readtype,fileObject.read(readsize))
                     except:
-                        raise error('Error!!!')
+                        break
                     udata = [fileObject.read(ind[col]) for col in xrange(colnum)]
                     for row in izip(*[cPickle.loads(zlib.decompress(udata[col])) for col in xrange(colnum)]) :
                         yield row
