2ae77ddbfbb9b04f9539e99d254c56d10b16ea4c
estama <estama@localhost6.localdomain6>
2014-11-11 19:05:55 +0200

    More robust jaydebe

--- src/lib/jaydebeapi/dbapi2.py
+++ src/lib/jaydebeapi/dbapi2.py
@@ -470,8 +470,8 @@ def _java_to_py(java_method):
         return getattr(java_val, java_method)()
     return to_py
 
-_to_double = lambda x: getattr(x, 'doubleValue')()
-_to_int = lambda x: getattr(x, 'intValue')()
+_to_double = lambda x: getattr(x, 'doubleValue', x)()
+_to_int = lambda x: getattr(x, 'intValue', x)()
 
 def _init_converters(types_map):
     """Prepares the converters for conversion of java types to python
