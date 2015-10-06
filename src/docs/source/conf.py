278436be7dc013724dd4cae2ec51dfa9e836e882
johnfouf <johnfouf@di.uoa.gr>
2015-10-06 14:10:53 +0300

    debug documentation

--- src/docs/source/conf.py
+++ src/docs/source/conf.py
@@ -19,7 +19,7 @@ import sys, os
 sys.path.append(os.path.abspath('.'))
 sys.path.append(os.path.abspath(os.path.dirname(__file__)+'../../../'))
 import functions
-
+extensions = [ "sphinxtogithub" ]
 #sys.path.append(os.path.abspath(os.path.dirname(__file__)+'\\..\\..\\'))
 # -- General configuration -----------------------------------------------------
 
