0ce8c0c97c70ff4428c9bbf5dee79c0ca68b17b0
johnfouf <johnfouf@di.uoa.gr>
2019-03-06 17:24:33 +0200

    update documentation

--- command_push.sh
+++
@@ -1,9 +0,0 @@
-git checkout gh-pages
-cd src/docs
-./builddocs.sh
-cd ../../
-cp -r src/docs/html/* .
-git add *
-git commit -am "update documentation"
-git push origin gh-pages
-git checkout master
